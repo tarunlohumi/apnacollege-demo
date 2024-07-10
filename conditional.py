d4c63b0a9ab18618bc0c97e2596721af332cdebb
Tarun Lohumi <tarunlohumi@gmail.com>
2024-07-10 00:57:41 -0700

    conditions code added

---
+++ conditional.py
@@ -0,0 +1,10 @@
+light = input("What is the light color(yellow/green/red): ")
+
+if(light == "green"):
+    print("Go")
+elif(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("Slow")
+else:
+    print("Light is broken")
